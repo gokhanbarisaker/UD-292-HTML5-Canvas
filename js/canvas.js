6145234f295d287bb6cf64a9c8502497229195c7
Gokhan Baris Aker <gokhanbarisaker@gmail.com>
2015-03-14 21:12:10 +0400

    Added image filter Invert && created Pixel object for easier data access

--- js/canvas.js
+++ js/canvas.js
@@ -18,6 +18,36 @@ ImageLoader.prototype.load = function (canvas, imageUrl, onLoad) {
 };
 
 
+// == Pixel ====================================================================
+
+var Pixel = function(r, g, b, a) {
+  Object.getPrototypeOf(Pixel.prototype).constructor.call(this);
+
+  this.r = r;
+  this.g = g;
+  this.b = b;
+  this.a = a;
+}
+
+Pixel.prototype.constructor = Pixel;
+Pixel.prototype.r = 0;
+Pixel.prototype.g = 0;
+Pixel.prototype.b = 0;
+Pixel.prototype.a = 0;
+Pixel.prototype.toGrayscale = function () {
+  var g = (0.299 * this.r) + (0.587 * this.g) + (0.114 * this.b);
+  return new Pixel(g, g, g, this.a);
+};
+Pixel.prototype.invert = function () {
+  return new Pixel(
+    255 - this.r,
+    255 - this.g,
+    255 - this.b,
+    this.a
+  );
+};
+
+
 // == Image filters ============================================================
 
 var ImageFilter = function() {
@@ -94,15 +124,15 @@ GrayscaleImageFilter.prototype.apply = function (canvas, onComplete) {
   var data = image.data;
   var length = data.length;
   var channelQuantity = this.channelQuantity;
-  var grayscale = 0;
+  var grayscalePixel;
 
   // Filter data
   for (var i = 0; i < length; i += channelQuantity)
   {
-    grayscale = this.convertGrayscale(data[i], data[i+1], data[i+2]);
-    data[i] = grayscale;
-    data[i+1] = grayscale;
-    data[i+2] = grayscale;
+    grayscalePixel = new Pixel(data[i], data[i+1], data[i+2], 255).toGrayscale();
+    data[i] = grayscalePixel.r;
+    data[i+1] = grayscalePixel.g;
+    data[i+2] = grayscalePixel.b;
   }
 
   // Update context image with filtered data
@@ -114,8 +144,44 @@ GrayscaleImageFilter.prototype.apply = function (canvas, onComplete) {
     onComplete();
   }
 };
-GrayscaleImageFilter.prototype.convertGrayscale = function (red, green, blue) {
-  return (0.299 * red) + (0.587 * green) + (0.114 * blue);
+
+
+// == Invert ==
+
+var InvertImageFilter = function() {
+  Object.getPrototypeOf(InvertImageFilter.prototype).constructor.call(this);
+};
+
+InvertImageFilter.prototype = Object.create(ImageFilter.prototype);
+InvertImageFilter.prototype.constructor = InvertImageFilter;
+InvertImageFilter.prototype.apply = function (canvas, onComplete) {
+  Object.getPrototypeOf(InvertImageFilter.prototype).apply.call(this, canvas);
+
+  // Get image data
+  var context = canvas.getContext('2d');
+  var image = context.getImageData(0, 0, canvas.width, canvas.height);
+  var data = image.data;
+  var length = data.length;
+  var channelQuantity = this.channelQuantity;
+  var invertedPixel;
+
+  // Filter data
+  for (var i = 0; i < length; i += channelQuantity)
+  {
+    invertedPixel = new Pixel(data[i], data[i+1], data[i+2], 255).invert();
+    data[i]   = invertedPixel.r;
+    data[i+1] = invertedPixel.g;
+    data[i+2] = invertedPixel.b;
+  }
+
+  // Update image data with filtered data
+  context.putImageData(image, 0, 0);
+
+  // Perform onCompleteCallback, if defined
+  if (onComplete != null)
+  {
+    onComplete();
+  }
 };
 
 
@@ -125,3 +191,4 @@ var imageLoader = new ImageLoader();
 // var imageFilter = new ImageFilter();
 var greenPixelImageFilter = new GreenPixelImageFilter();
 var grayscaleImageFilter = new GrayscaleImageFilter();
+var invertImageFilter = new InvertImageFilter();
